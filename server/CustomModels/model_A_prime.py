eae7f5c830352fcf32ec00fefbb7aa1b998ea11e
Charlie Wang <chongyu.wang88@gmail.com>
2024-06-17 16:28:15 -0400

    model A prime

--- server/CustomModels/model_A_prime.py
+++ server/CustomModels/model_A_prime.py
@@ -6,7 +6,7 @@ I will give you a description of a person in a json format in the following form
 name: person's name,
 age: some number,
 gender: (male, female, or prefer not to say),
-primary_reason_for_self_cloning: [tag1, tag2, tag3,...,tagn]
+primary_reason: [tag1, tag2, tag3,...,tagn]
 }
 
 and you will give me 10 interview questions in a json format  to gauge the person's personality and life story. 
